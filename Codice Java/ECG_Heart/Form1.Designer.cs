2b611953f6ff9b495155c8c638f90a344de300d4
NicoMarinoV <96552280+NicoVMari@users.noreply.github.com>
2022-09-15 16:37:53 +0200

    Add files via upload

---
+++ Codice Java/ECG_Heart/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace ECG_Heart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbmbound = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbporte = new System.Windows.Forms.ComboBox();
+            this.btndiscconnect = new System.Windows.Forms.Button();
+            this.btnconnect = new System.Windows.Forms.Button();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbmbound);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbporte);
+            this.groupBox1.Controls.Add(this.btndiscconnect);
+            this.groupBox1.Controls.Add(this.btnconnect);
+            this.groupBox1.Font = new System.Drawing.Font("Javanese Text", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1303, 174);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serial Connection";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(7, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 45);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Baund Rate : ";
+            // 
+            // cbmbound
+            // 
+            this.cbmbound.BackColor = System.Drawing.Color.White;
+            this.cbmbound.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbmbound.ForeColor = System.Drawing.Color.Black;
+            this.cbmbound.FormattingEnabled = true;
+            this.cbmbound.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.cbmbound.Location = new System.Drawing.Point(195, 102);
+            this.cbmbound.Name = "cbmbound";
+            this.cbmbound.Size = new System.Drawing.Size(110, 33);
+            this.cbmbound.TabIndex = 6;
+            this.cbmbound.Text = "9600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(7, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 45);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "COM : ";
+            // 
+            // cmbporte
+            // 
+            this.cmbporte.BackColor = System.Drawing.Color.White;
+            this.cmbporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbporte.ForeColor = System.Drawing.Color.Black;
+            this.cmbporte.FormattingEnabled = true;
+            this.cmbporte.Location = new System.Drawing.Point(195, 53);
+            this.cmbporte.Name = "cmbporte";
+            this.cmbporte.Size = new System.Drawing.Size(110, 33);
+            this.cmbporte.TabIndex = 4;
+            this.cmbporte.DropDown += new System.EventHandler(this.cmbporte_DropDown);
+            // 
+            // btndiscconnect
+            // 
+            this.btndiscconnect.BackColor = System.Drawing.Color.White;
+            this.btndiscconnect.Location = new System.Drawing.Point(1002, 93);
+            this.btndiscconnect.Name = "btndiscconnect";
+            this.btndiscconnect.Size = new System.Drawing.Size(283, 60);
+            this.btndiscconnect.TabIndex = 3;
+            this.btndiscconnect.Text = "DISCONNECT";
+            this.btndiscconnect.UseVisualStyleBackColor = false;
+            this.btndiscconnect.Click += new System.EventHandler(this.btndiscconnect_Click);
+            // 
+            // btnconnect
+            // 
+            this.btnconnect.BackColor = System.Drawing.Color.White;
+            this.btnconnect.Location = new System.Drawing.Point(1002, 26);
+            this.btnconnect.Name = "btnconnect";
+            this.btnconnect.Size = new System.Drawing.Size(283, 60);
+            this.btnconnect.TabIndex = 2;
+            this.btnconnect.Text = "CONNECT";
+            this.btnconnect.UseVisualStyleBackColor = false;
+            this.btnconnect.Click += new System.EventHandler(this.btnconnect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chart1);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(12, 192);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1303, 654);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Black;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea1.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea1.BackColor = System.Drawing.Color.Black;
+            chartArea1.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.ShadowColor = System.Drawing.Color.Silver;
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(15, 21);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.Lime;
+            series1.Legend = "Legend1";
+            series1.Name = "Data";
+            series1.YValuesPerPoint = 4;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1270, 610);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1345, 858);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Form1";
+            this.Text = "ECG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbporte;
+        private System.Windows.Forms.Button btndiscconnect;
+        private System.Windows.Forms.Button btnconnect;
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbmbound;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Codice Java/ECG_Heart/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ECG_Heart
+{
+    public partial class Form1 : Form
+    {
+        delegate void serialCalback(string val);
+        double x;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btndiscconnect.Enabled = false;
+            btnconnect.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            chart1.Series[0].IsVisibleInLegend = false; 
+        }
+
+        private void cmbporte_DropDown(object sender, EventArgs e)
+        {
+            string[] portList = SerialPort.GetPortNames();
+            cmbporte.Items.Clear();
+            cmbporte.Items.AddRange(portList);
+
+        }
+
+        private void btnconnect_Click(object sender, EventArgs e)
+        {
+            if (!serialPort.IsOpen)
+            {
+                try
+                {
+                    serialPort.PortName = cmbporte.Text;
+                    serialPort.BaudRate = Convert.ToInt32(cbmbound.Text);
+                    serialPort.Open();
+
+                    MessageBox.Show("Connessione Arduino");
+                    btndiscconnect.Enabled = true;
+                    btnconnect.Enabled = false;
+                    chart1.Series[0].Points.Clear();
+                    timer1.Start();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+
+        private void btndiscconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                try
+                {
+                    timer1.Stop();
+                    serialPort.Close();
+
+                    MessageBox.Show("Disconnessione Arduino");
+                    btndiscconnect.Enabled = false;
+                    btnconnect.Enabled = true;
+                    chart1.Series[0].Points.Clear();
+                    
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }               
+            }
+        }
+
+        private void chartsetText(string val)
+        {
+            if (this.chart1.InvokeRequired)
+            {
+                serialCalback scb = new serialCalback(chartsetText);
+                this.BeginInvoke(scb, new object[] { val });
+            }
+            else
+            {
+                if(!String.Equals("!",val))
+                {
+                    chart1.Series[0].Points.AddXY(x, val);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string incomString = serialPort.ReadLine();
+            chartsetText(incomString);
+
+            //if(String.Equals(incomString,"0"))
+            //{
+            //    serialPort.Write("0");
+            //}
+
+            if (chart1.Series[0].Points.Count > 100) chart1.Series[0].Points.RemoveAt(0);
+
+            chart1.ChartAreas[0].AxisX.Minimum = chart1.Series[0].Points[0].XValue;
+            chart1.ChartAreas[0].AxisX.Maximum = x;
+
+            x += 1;
+        }
+    }
+}
